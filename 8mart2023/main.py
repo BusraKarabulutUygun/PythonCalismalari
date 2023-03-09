4e611f6cdc174582bb0ec20b221ecf3b800ceea0
BusraKarabulutUygun <127445649+BusraKarabulutUygun@users.noreply.github.com>
2023-03-09 23:56:21 +0300

    first commit

---
+++ 8mart2023/main.py
@@ -0,0 +1,29 @@
+# ekrana yazı yazdırma
+print("hello word")
+
+#int , float, string, bool
+sayi_int = 1
+sayi_float = 1.0
+sayi_string = "1"
+sayi1 = True
+
+print(sayi_int)
+print(sayi_float)
+print(sayi_string)
+
+#toplama, çıkarma, bölme ve mod
+sayi1 = 10
+sayi2 = 5
+sayi_toplama = sayi1 + sayi2
+sayi_cikarma = sayi1 - sayi2
+sayi_bolme = sayi1 / sayi2
+sayi_mod = sayi1 % sayi2 
+
+#if conditions 
+if sayi1 > sayi2:
+    print("sayi1 büyüktür sayi2 den" )
+elif sayi1 < sayi2:
+    print( "sayi1 küçüktür sayi2 den")
+else:
+    print("diger ihtimaller :)")
+
